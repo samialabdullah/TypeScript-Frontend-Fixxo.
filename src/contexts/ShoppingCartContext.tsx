2e180f2b1f19300a1ccbc5e566fe440971fd386e
Sami Alabdalah <samialabdalah@yahoo.com>
2022-12-25 20:38:51 +0100

    work with ts

--- src/contexts/ShoppingCartContext.tsx
+++ src/contexts/ShoppingCartContext.tsx
@@ -2,77 +2,46 @@ import ShoppingCart from "../components/ShoppingCart";
 import { ProductProduct } from "../models/ProductModels";
 import { createContext, useContext, useState } from "react";
 
-
  export interface ShoppingCartContextModel {
-
     articleNumber?: string
-
     quantity?: number | null
-
     cartproducts?: any
-
     totalQuantity: number
-
     getproductQuantity: any
-
     increment: any
-
     decrement: any
-
     removeproduct: any
-
 }
-
   interface IShoppingCartProps {
     children: any
 }
 
-
-
 export const ShoppingCartContext = createContext<ShoppingCartContextModel | null>(null)
-
-
-
 
 export const useShoppingCart = () => {
     return useContext(ShoppingCartContext)
 }
 
-
-
 export const ShoppingCartProvider = ({children} : IShoppingCartProps ) => {
-
     const [cartproducts, setCartproducts ] = useState <any[]>([])
+        const totalQuantity = cartproducts.reduce(
+            
+            (quantity: number, product: { quantity: number; }) => product.quantity + quantity, 0
 
-    const totalQuantity = cartproducts.reduce(
-
-        (quantity: number, product: { quantity: number; }) => product.quantity + quantity, 0
-
-    )
-
-
+        )
 
     const getproductQuantity = (articleNumber: string) => {
-
         return cartproducts.find((product: { articleNumber: string; }) => product.articleNumber  === articleNumber)?.quantity || 0
-
     }
 
-
-
     const increment = (cartproducts: { articleNumber: string; product: ProductProduct; }) => {
-
         const {articleNumber, product} = cartproducts
-
-
 
         setCartproducts((products: any[]) => {
 
             if (products.find((product: { articleNumber: string; }) => product.articleNumber === articleNumber) == null) {
 
                 return [...products,{articleNumber, product, quantity: 1 }]
-
-
 
             }else {
 
@@ -85,32 +54,21 @@ export const ShoppingCartProvider = ({children} : IShoppingCartProps ) => {
                     }else {
 
                         return product
-
                     }
-
                 })
-
             }
-
         })
-
     }
-
-
 
     const decrement = (cartproducts: { articleNumber: string; }) => {
 
         const {articleNumber} = cartproducts
-
-
 
         setCartproducts((products: any[]) => {
 
             if (products.find((product: { articleNumber: string; }) => product.articleNumber === articleNumber).quantity === 1) {
 
                 return products.filter((product: { articleNumber: string; }) => product.articleNumber !== articleNumber)
-
-
 
             }else {
 
@@ -121,39 +79,21 @@ export const ShoppingCartProvider = ({children} : IShoppingCartProps ) => {
                         return {...product, quantity: product.quantity - 1}
 
                     }else {
-
                         return product
-
                     }
-
                 })
-
             }
-
         })
-
     }
 
     const removeproduct = (articleNumber: string) => {
-
         setCartproducts((product: any[]) => {
-
             return product.filter((product: { articleNumber:string; }) => product.articleNumber !== articleNumber)
-
         })
-
     }
 
-
-
     return <ShoppingCartContext.Provider value={{cartproducts, totalQuantity, getproductQuantity, increment, decrement, removeproduct}}>
-
         {children}
-
         <ShoppingCart/>
-
-
-
     </ShoppingCartContext.Provider>
-
 }
